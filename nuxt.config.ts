f925753d16839cdba003f2bf7185ba54b5eece47
SashaLop-web <sashahalp05@gmail.com>
2025-06-08 17:12:27 +0300

    nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,39 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-import 'dotenv/config';
-export default defineNuxtConfig({
-	css: ['@/assets/styles/main.scss'],
-	vite: {
-		ssr: {
-			noExternal: ['sqlite3'], // Форсируем обработку модуля
-		},
-	},
-	runtimeConfig: {
-		jwtSecret: process.env.JWT_SECRET || 'default_fallback_key',
-		public: {
-			// Публичные переменные (если нужны)
-		},
-	},
-	modules: [
-		'@pinia/nuxt', // Добавляем Pinia как модуль
-	],
-	plugins: [
-		'~/plugins/auth.ts', // Явное подключение плагина
-	],
-	compatibilityDate: '2024-11-01',
+import 'dotenv/config'
 
-	devtools: { enabled: true },
+export default defineNuxtConfig({
+  css: ['@/assets/styles/main.scss'],
+
+  vite: {
+    assetsInclude: ['**/*.woff', '**/*.woff2'], // Для подключения шрифтов
+    ssr: {
+      noExternal: ['sqlite3']
+    }
+  },
+
+  runtimeConfig: {
+    jwtSecret: process.env.JWT_SECRET || 'default_fallback_key',
+    public: {
+      // Публичные переменные при необходимости
+    }
+  },
+
+  modules: [
+    '@pinia/nuxt'
+  ],
+
+  plugins: [
+    '~/plugins/auth.ts'
+  ],
+
+  alias: {
+    // Чтобы ~assets работал в SCSS и других местах
+    '~assets': '/assets',
+    '~fonts': '/assets/fonts'
+  },
+
+  compatibilityDate: '2024-11-01',
+
+  devtools: {
+    enabled: true
+  }
 })
